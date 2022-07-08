71ca836380c4d67befc09093afc374e100d2a5d6
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-07-08 21:23:09 +0300

    start

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+
+@app.route("/perform_query")
+def perform_query():
+    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
+    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
+    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
+    # вернуть пользователю сформированный результат
+    return app.response_class('', content_type="text/plain")
